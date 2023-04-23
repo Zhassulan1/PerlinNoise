70b0f5300be82c55f91135bf7f15e148445e789e
Zhassulan Kainazarov <zhkainazarov@gmail.com>
2023-04-23 23:23:53 +0600

    Added usage example

--- Usage.py
+++ Usage.py
@@ -11,21 +11,21 @@ parameters = [
 ]
 
 
-size_x, size_y = 500, 500                                       # if it calculates too long try 100, 100
+size_x, size_y = 500, 500                                               # if it calculates too long try 100, 100
 noise = Perlin(size_x, size_y)
 
 
-noise_example_1 = noise.multiParameterNoise(parameters)
+noise_example_1 = noise.multiParameterNoise(parameters)                 # multiple noises with different octaves
 
 noise_example_2 = np.zeros((size_y, size_x))
 for i in range(size_y):
     for j in range(size_x):
-        noise_example_2[i][j] = noise.multioctaveNoise(i, j, 6)
+        noise_example_2[i][j] = noise.multioctaveNoise(i, j, octaves=6) # multiple octaves
 
 noise_example_3 = np.zeros((size_y, size_x))
 for i in range(size_y):
     for j in range(size_x):
-        noise_example_3[i][j] = noise.Noise(i, j)               # <-only one octave
+        noise_example_3[i][j] = noise.Noise(i, j)                       # <-only one octave
 
 
 # Plot the noise values
